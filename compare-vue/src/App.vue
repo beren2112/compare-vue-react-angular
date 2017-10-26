1700b622eb4a575755890e8b4ed093a6f9042e71
Konstantinos Angeletopoulos <kostasAngels@outlook.com>
2017-10-26 11:25:21 +0300

    Initial Commit

---
+++ compare-vue/src/App.vue
@@ -0,0 +1,116 @@
+<template>
+  <div id="app">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="container">
+        <div class="navbar-brand">
+          <router-link to="/" class="navbar-item">MyCompany</router-link>
+          <button class="button navbar-burger" v-on:click="toggleNav" v-bind:class="{'is-active': isActive}">
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
+        <div class="navbar-menu navbar-end" v-bind:class="{'is-active': isActive}">
+          <router-link to="/" class="navbar-item r-item">Home</router-link>
+          <router-link to="faq" class="navbar-item r-item">Features</router-link>
+          <router-link to="faq" class="navbar-item r-item">About</router-link>
+          <router-link to="faq" class="navbar-item r-item">Faq</router-link>
+
+          <div class="navbar-item">
+            <p class="control">
+              <a class="button is-primary is-outlined">
+                <span class="icon">
+                  <i class="fa fa-download"></i>
+                </span>
+                <span> Join Now</span>
+              </a>
+            </p>
+          </div>
+
+        </div>
+      </div>
+    </nav>
+
+    <router-view/>
+
+    <footer class="footer is-primary">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <p>And this right here is a spiffy footer, where you can put stuff.</p>
+          </div>
+          <div class="column has-text-right">
+            <a class="icon" href="#"><i class="fa fa-facebook"></i></a>
+            <a class="icon" href="#"><i class="fa fa-twitter"></i></a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data: function(){
+    return{
+      isActive: false
+    }
+  },
+  methods: {
+    toggleNav: function(){
+      this.isActive = !this.isActive;
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+@import "../node_modules/bulma/bulma.sass"
+@import "mq"
+
+.is-primary a
+  color:  #1EC9AC
+
+.navbar
+  background-color: #383838
+  a:hover
+    color: gray
+
+.navbar-brand a
+  color: #fff
+  font-weight: bold
+  +desktop
+    padding-left: 0
+
+a.r-item
+  color: #c1c1c1
+  padding: 0.5rem 1.75rem
+  background: transparent!important
+  &:hover
+    color: #f9f9f9
+  +mobile
+    color: gray!important
+    &:hover
+      background-color: #f1f1f1
+
+.navbar-menu.is-active a
+  &:hover
+    color: gray!important
+
+nav .navbar-burger, nav .navbar-burger.is-active
+  color: #1EC9AC!important
+  background-color: transparent
+  border: 0
+  box-shadow: none
+
+footer
+  background-color: $primary !important
+  color:  #fff
+
+  .icon
+    color: #fff
+    margin-left: 20px
+
+
+</style>
---
+++ compare-vue/src/components/Faq.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class='faq'>
+      <div class='container'>
+        <section class='section pd-top-bot-only'>
+          <h1 class='title'>FAQ</h1>
+          <h2 class='subtitle is-4'>Lorem isum dolor sit amet and all of that jazz</h2>
+
+          <div class='columns' v-if='faqs && faqs.length'>
+              <div class='column is-one-third' v-for='faq of faqs'>
+                <div class='card'>
+                  <div class='card-content'>
+                    <p class='title'>{{ faq.title }}</p>
+                    <p class='answer'>{{ faq.body }}</p>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </section>
+      </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'faq',
+  data: () =>({
+    faqs: [],
+    errors: []
+  }),
+
+  created() {
+    axios.get('http://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        this.faqs = response.data.slice(0,12);
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+  }
+}
+</script>
+
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
+<style lang='sass' scoped>
+@import '../mq'
+
+.pd-top-bot-only
+  padding-left: 0
+  padding-right : 0
+
+.pd
+  padding: 2em 0 1.5em 0
+
+.answer
+  margin-top: 10px !important
+  color: gray
+
+.columns
+  flex-wrap: wrap
+
+.card
+  height: 100%
+
+section
+  +mobile
+    padding-left: 15px
+    padding-right: 15px
+
+
+</style>
